4d9874b928ac0672f6454fd0abaea99e3af1b9aa
MPHATSO MATEMBA <BSC/COM/NE/04/19@COMLAB.COM>
2024-04-22 19:56:13 +0200

    initialized library

--- Tracking/Tracking/Tracking.ino
+++ Tracking/Tracking/Tracking.ino
@@ -7,14 +7,17 @@ LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 static const int RXpin = 4, TXpin = 3;
 static const uint32_t GPSBaud = 9600;
 
+TinyGPSPlus gps;
+int temp = 0,i;
 
+SoftwareSerial ss(RXpin, TXpin);
+String stringval = "";
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
