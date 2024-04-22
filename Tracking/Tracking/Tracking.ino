593ee978322bd5f980baab886d17f53e54287476
MPHATSO MATEMBA <BSC/COM/NE/04/19@COMLAB.COM>
2024-04-22 19:49:10 +0200

    created sketch

---
+++ Tracking/Tracking/Tracking.ino
@@ -0,0 +1,20 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+static const int RXpin = 4, TXpin = 3;
+static const uint32_t GPSBaud = 9600;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
